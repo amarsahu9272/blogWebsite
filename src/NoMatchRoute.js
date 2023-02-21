faebaef1c6b82b20911ac0593480bc94b56f4153
Anil Singh <anil927a@gmail.com>
2023-02-21 17:51:59 +0530

    NoMatchRoute/page

--- src/NoMatchRoute.js
+++ src/NoMatchRoute.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import "./index.css";
 
 function NoMatchRoute() {
   return (
-    <div>
-      Oops page NOT found
+    <div className="noMatchRoute">
+      <div>
+        <span>404. That's an error</span>
+        <p>The requested URL was not found on this server. </p>
+        <p>That’s all we know.</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default NoMatchRoute
+export default NoMatchRoute;
